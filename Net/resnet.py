a335d21bf813f1657c6be1592d2bcee01a957885
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-12-06 20:29:49 +0200

    Update resnet.py

--- Net/resnet.py
+++ Net/resnet.py
@@ -74,7 +74,8 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.fc = nn.Linear(512*block.expansion, num_classes)
-        self.temp = temp
+        #self.temp = temp
+        self.temp = nn.Parameter(torch.zeros((1, num_classes)), requires_grad=True)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
