b0ccb0dda0b213421f3f113aa2ff8cde5844f774
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-12-08 17:03:32 +0200

    Update metrics.py

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -175,18 +175,14 @@ class ECELoss(nn.Module):
         confidences, predictions = torch.max(softmaxes, 1)
         accuracies = predictions.eq(labels)
 
-        #ece = torch.zeros(1, device=logits.device)
-        ece = torch.zeros(logits.size()[1], device=logits.device)
+        ece = torch.zeros(1, device=logits.device)
         for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
             # Calculated |confidence - accuracy| in each bin
             in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
             prop_in_bin = in_bin.float().mean()
             if prop_in_bin.item() > 0:
-                #accuracy_in_bin = accuracies[in_bin].float().mean()
-                #avg_confidence_in_bin = confidences[in_bin].mean()
                 accuracy_in_bin = accuracies[in_bin].float().mean()
                 avg_confidence_in_bin = confidences[in_bin].mean()
-                print((avg_confidence_in_bin - accuracy_in_bin).size(), prop_in_bin.size())
                 ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
 
         return ece
