66172a28936aa7dee91bbf09f10b226afd1f7ed8
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-12-13 10:40:35 +0200

    Update metrics.py

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -256,3 +256,39 @@ class ClasswiseECELoss(nn.Module):
 
         sce = torch.mean(per_class_sce)
         return sce
+    
+  
+class ClassECELoss(nn.Module):
+    '''
+    Compute Classwise ECE
+    '''
+    def __init__(self, n_bins=15):
+        super(ClasswiseECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        num_classes = int((torch.max(labels) + 1).item())
+        softmaxes = F.softmax(logits, dim=1)
+        per_class_sce = None
+
+        for i in range(num_classes):
+            class_confidences = softmaxes[:, i]
+            class_sce = torch.zeros(1, device=logits.device)
+            labels_in_class = labels.eq(i) # one-hot vector of all positions where the label belongs to the class i
+
+            for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+                in_bin = class_confidences.gt(bin_lower.item()) * class_confidences.le(bin_upper.item())
+                prop_in_bin = in_bin.float().mean()
+                if prop_in_bin.item() > 0:
+                    accuracy_in_bin = labels_in_class[in_bin].float().mean()
+                    avg_confidence_in_bin = class_confidences[in_bin].mean()
+                    class_sce += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+            if (i == 0):
+                per_class_sce = class_sce
+            else:
+                per_class_sce = torch.cat((per_class_sce, class_sce), dim=0)
+
+        return per_class_sce
