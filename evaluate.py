85e2e9a5dd1583368e84bf1fb716c77a35ac3d24
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-12-24 18:05:52 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
 
     if create_plots:
         plt.figure()
-        plt.plot(p_acc, p_csece.cpu())
+        plt.scatter(p_acc, p_csece.cpu())
         plt.plot(p_acc, p_ece*torch.ones(len(p_acc)))
         #plt.legend('temperature scaling ECE', 'constant temperature ECE')
         plt.title('Classes ECE vs. classes accuracy before scaling, {}, {}'.format(dataset, args.model))
@@ -294,7 +294,7 @@ if __name__ == "__main__":
 
     if create_plots:    
         plt.figure()
-        plt.plot(accuracies, csece.cpu())
+        plt.scatter(accuracies, csece.cpu())
         plt.plot(accuracies, ece*torch.ones(len(accuracies)))
         #plt.legend('temperature scaling ECE', 'constant temperature ECE')
         plt.title('Classes ECE vs. classes accuracy after scaling, {}, {}'.format(dataset, args.model))
