ac0d92f8b93ce5f1a001e052afff870eb4fb12de
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2021-01-27 10:20:21 +0200

    Update metrics.py

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -428,4 +428,36 @@ class binsECELoss(nn.Module):
         print('low bins: ', torch.mean(torch.FloatTensor(low_bins)).item())
 
         return per_class_sce_high, per_class_sce_low, classes_acc
+    
+# Calibration error scores in the form of loss metrics
+class diffECELoss(nn.Module):
+    '''
+    Compute ECE (Expected Calibration Error)
+    '''
+    def __init__(self, n_bins=15):
+        super(diffECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+        bin_over_confidence = []
+        bins = []
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                bin_over_confidence.append((avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin).item())
+                bins.append(bin_lower.item())
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece, bin_over_confidence, bins
 
