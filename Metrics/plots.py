bc05018bf5ee0a0a8fb299d6838b90cdbefdb294
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2021-01-27 10:29:02 +0200

    Update plots.py

--- Metrics/plots.py
+++ Metrics/plots.py
@@ -193,3 +193,12 @@ def diff_ece_plot(acc, csece1, csece2, save_plots_loc, dataset, model, trained_l
         plt.savefig(os.path.join(save_plots_loc, '{}_{}'.format(dataset, model), 'diff_{}_ece_acc_after_scaling_{}_{}_{}_acc.pdf'.format(scaling_type, dataset, model, trained_loss)), dpi=40)
     else:
         plt.savefig(os.path.join(save_plots_loc, '{}_{}'.format(dataset, model), 'diff_{}_ece_acc_after_scaling_{}_{}_{}.pdf'.format(scaling_type, dataset, model, trained_loss)), dpi=40)
+        
+def bins_over_confidence(bins, diff, save_plots_loc, dataset, model, trained_loss, scaling_related='before'):
+    plt.figure()
+    plt.plt(bins, diff.cpu())
+    plt.xlabel('bins', fontsize=10)
+    plt.xticks(fontsize=10)
+    plt.ylabel('confidence - accuracy', fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.savefig(os.path.join(save_plots_loc, '{}_{}'.format(dataset, model), 'over_conf_bins_{}_scaling_{}_{}_{}.pdf'.format(scaling_related, dataset, model, trained_loss)), dpi=40)
