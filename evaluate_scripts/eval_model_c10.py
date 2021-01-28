ee8d1b1d057328ee034f3687ef5aea91668a8eb7
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2021-01-28 11:19:57 +0200

    Add files via upload

--- evaluate_scripts/eval_model_c10.py
+++ evaluate_scripts/eval_model_c10.py
@@ -1,17 +1,20 @@
-# Load in model weights and evaluate its goodness (ECE, MCE, error) also saves logits.
-# ResNet model from https://github.com/BIGBALLON/cifar-10-cnn/blob/master/4_Residual_Network/ResNet_keras.py
+# Load in pre-trained model weights and evaluate its goodness (ECE, MCE, error) also saves logits.
 
-import keras
+from __future__ import print_function
+
+import os.path
+
+import densenet
 import numpy as np
+import sklearn.metrics as metrics
+
 from keras.datasets import cifar10, cifar100
+from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Conv2D, Dense, Input, add, Activation, GlobalAveragePooling2D
-from keras.callbacks import LearningRateScheduler, TensorBoard, ModelCheckpoint
-from keras.models import Model
-from keras import optimizers, regularizers
+from keras.optimizers import SGD
+from keras.callbacks import LearningRateScheduler
+from keras import backend as K
 from sklearn.model_selection import train_test_split
-import pickle
 
 # Imports to get "utility" package
 import sys
@@ -19,113 +22,62 @@ from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath("utility") ) ) )
 from utility.evaluation import evaluate_model
 
+batch_size = 64
+nb_classes10 = 10
+nb_classes100 = 100
 
-stack_n            = 18            
-num_classes10      = 10
-num_classes100     = 100
+nb_epoch = 300
+
 img_rows, img_cols = 32, 32
-img_channels       = 3
-batch_size         = 128
-epochs             = 200
-iterations         = 45000 // batch_size
-weight_decay       = 0.0001
-mean = [125.307, 122.95, 113.865]  # Mean (per-pixel mean?) - let it be atm
-std  = [62.9932, 62.0887, 66.7048]
+img_channels = 3
+
+img_dim = (img_channels, img_rows, img_cols) if K.image_dim_ordering() == "th" else (img_rows, img_cols, img_channels)
+depth = 40
+nb_dense_block = 3
+growth_rate = 12
+nb_filter = -1
+dropout_rate = 0.0 # 0.0 for data augmentation
 seed = 333
-weights_file_10 = "../../models/resnet_110_45k_c10.h5"
-weights_file_100 = "../../models/resnet_110_45k_c100.h5"
+weight_decay = 0.0001
+learning_rate = 0.1
 
-def scheduler(epoch):
-    if epoch < 80:
-        return 0.1
-    if epoch < 150:
-        return 0.01
-    return 0.001
+weights_file_10 =  "../../models/weights_densenet_40_c10.h5"
+weights_file_100 =  "../../models/weights_densenet_40_c100.h5"
 
-def residual_network(img_input,classes_num=10,stack_n=5):
-    def residual_block(intput,out_channel,increase=False):
-        if increase:
-            stride = (2,2)
-        else:
-            stride = (1,1)
 
-        pre_bn   = BatchNormalization()(intput)
-        pre_relu = Activation('relu')(pre_bn)
+# Preprocessing for DenseNet https://arxiv.org/pdf/1608.06993v3.pdf
+def color_preprocessing(x_train,x_test):
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    mean = [125.307, 122.95, 113.865]
+    std  = [62.9932, 62.0887, 66.7048]
+    for i in range(3):
+        x_train[:,:,:,i] = (x_train[:,:,:,i] - mean[i]) / std[i]
+        x_test[:,:,:,i] = (x_test[:,:,:,i] - mean[i]) / std[i]
 
-        conv_1 = Conv2D(out_channel,kernel_size=(3,3),strides=stride,padding='same',
-                        kernel_initializer="he_normal",
-                        kernel_regularizer=regularizers.l2(weight_decay))(pre_relu)
-        bn_1   = BatchNormalization()(conv_1)
-        relu1  = Activation('relu')(bn_1)
-        conv_2 = Conv2D(out_channel,kernel_size=(3,3),strides=(1,1),padding='same',
-                        kernel_initializer="he_normal",
-                        kernel_regularizer=regularizers.l2(weight_decay))(relu1)
-        if increase:
-            projection = Conv2D(out_channel,
-                                kernel_size=(1,1),
-                                strides=(2,2),
-                                padding='same',
-                                kernel_initializer="he_normal",
-                                kernel_regularizer=regularizers.l2(weight_decay))(intput)
-            block = add([conv_2, projection])
-        else:
-            block = add([intput,conv_2])
-        return block
-
-    # build model
-    # total layers = stack_n * 3 * 2 + 2
-    # stack_n = 5 by default, total layers = 32
-    # input: 32x32x3 output: 32x32x16
-    x = Conv2D(filters=16,kernel_size=(3,3),strides=(1,1),padding='same',
-               kernel_initializer="he_normal",
-               kernel_regularizer=regularizers.l2(weight_decay))(img_input)
-
-    # input: 32x32x16 output: 32x32x16
-    for _ in range(stack_n):
-        x = residual_block(x,16,False)
-
-    # input: 32x32x16 output: 16x16x32
-    x = residual_block(x,32,True)
-    for _ in range(1,stack_n):
-        x = residual_block(x,32,False)
-    
-    # input: 16x16x32 output: 8x8x64
-    x = residual_block(x,64,True)
-    for _ in range(1,stack_n):
-        x = residual_block(x,64,False)
-
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = GlobalAveragePooling2D()(x)
-
-    # input: 64 output: 10
-    x = Dense(classes_num,activation='softmax',
-              kernel_initializer="he_normal",
-              kernel_regularizer=regularizers.l2(weight_decay))(x)
-    return x
+    return x_train, x_test
 
 
 if __name__ == '__main__':
 
-    # load data
-    print("Cifar-10 evaluation")
-        
+    # CIFAR-10 ===================
+
+    print("Evaluate CIFAR-10 densenet.")
+
+    model = densenet.DenseNet(img_dim, classes=nb_classes10, depth=depth, nb_dense_block=nb_dense_block,
+                              growth_rate=growth_rate, nb_filter=nb_filter, dropout_rate=dropout_rate, weights=None, weight_decay=1e-4)
+
+                              
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    y_test = keras.utils.to_categorical(y_test, num_classes10)
-    
-    # color preprocessing - using precalculated means and std-s
+    #For data preprocessing, we normalize the data using the channel means and standard deviations (https://arxiv.org/pdf/1608.06993v3.pdf)
+    x_train, x_test = color_preprocessing(x_train, x_test)
+
+
     x_train45, x_val, y_train45, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=seed)  # random_state = seed
-    
-    img_mean = x_train45.mean(axis=0)  # per-pixel mean
-    img_std = x_train45.std(axis=0)
-    x_train45 = (x_train45-img_mean)/img_std
-    x_val = (x_val-img_mean)/img_std
-    x_test = (x_test-img_mean)/img_std
-    
-    # build network
-    img_input = Input(shape=(img_rows,img_cols,img_channels))
-    output    = residual_network(img_input,num_classes10,stack_n)
-    model    = Model(img_input, output)    
+
+
+    y_train45 = np_utils.to_categorical(y_train45, nb_classes10)  # 1-hot vector
+    y_val = np_utils.to_categorical(y_val, nb_classes10)
+    y_test = np_utils.to_categorical(y_test, nb_classes10)
     evaluate_model(model, weights_file_10, x_test, y_test, bins = 15, verbose = True, 
-                   pickle_file = "probs_resnet110_c10", x_val = x_val, y_val = y_val)
-    
+                   pickle_file = "probs_densenet40_c10", x_val = x_val, y_val = y_val)
---
+++ evaluate_scripts/eval_models.py
@@ -0,0 +1,74 @@
+# Load in pre-trained model weights and evaluate its goodness (ECE, MCE, error) also saves logits.
+
+
+import numpy as np
+   
+from keras.optimizers import SGD
+from densenet161 import DenseNet 
+from load_data_imagenet import load_data_imagenet_split
+from resnet152 import resnet152_model
+import keras
+
+# Imports to get "utility" package
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath("utility") ) ) )
+from utility.evaluation import evaluate_model
+
+# Constants, predifined means for image net
+MEAN = [103.939, 116.779, 123.68]
+
+if __name__ == '__main__':
+
+    print("Evaluate ResNet152")
+
+    ## Load already split and resized data (mean subtracted)
+    seed = 333
+    num_classes = 1000
+    weights_file_resnet = "../../models/resnet152_weights_tf.h5"
+    weights_file_densenet = "../../models/densenet161_weights_tf.h5"
+
+
+    (x_val, y_val), (x_test, y_test) = load_data_imagenet_split(seed = seed)
+    
+    print("Data loaded.")
+
+    y_val = keras.utils.to_categorical(y_val, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    print("Preprocess data.")
+    
+    x_test = x_test[..., ::-1]  # What this does? Changes the order of last layer - RGB to BGR
+    x_val = x_val[..., ::-1]
+    
+    for i in range(3):
+        x_test[:,:,:,i] -= MEAN[i]
+        x_val[:,:,:,i] -= MEAN[i]
+
+    model = resnet152_model()
+    sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    print("Start evaluation!")
+    evaluate_model(model, weights_file_resnet, x_test, y_test, bins = 15, verbose = True, 
+                   pickle_file = "probs_resnet152_imgnet", x_val = x_val, y_val = y_val)
+    
+
+    print("Evaluate DenseNet161")
+    
+    model = DenseNet(reduction=0.5, classes=1000)
+
+    sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Subtract mean pixel and multiple by scaling constant # pixel mean subtracted previously
+    # Reference: https://github.com/shicai/DenseNet-Caffe
+    
+    for i in range(3):
+        x_test[:,:,:,i] = x_test[:,:,:,i] * 0.017
+        x_val[:,:,:,i] = x_val[:,:,:,i] * 0.017
+    
+    print("Evaluation for second model.")
+    evaluate_model(model, weights_file_densenet, x_test, y_test, bins = 15, verbose = True, 
+                   pickle_file = "probs_densenet161_imgnet", x_val = x_val, y_val = y_val)
+
