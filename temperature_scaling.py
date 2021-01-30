77beabf11aa65e75ef65539e8523718b429f62de
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2021-01-30 21:30:46 +0200

    Update temperature_scaling.py

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -344,7 +344,7 @@ def set_temperature2(logits, labels, iters=1, cross_validate='ece',
         confs = np.max(softmaxs, axis=1)
         ece_list.append(ECE(confs, preds, labels, bin_size = 1/num_bins))
         """
-        ece_list.append(ece_criterion(class_temperature_scale2(logits), labels).item())
+        ece_list.append(ece_criterion(class_temperature_scale2(logits, csece_temperature), labels).item())
         if acc_check:
             _, temp_accuracy, _, _, _ = test_classification_net_logits(class_temperature_scale2(logits, csece_temperature), labels)
             if temp_accuracy >= accuracy:
@@ -371,7 +371,7 @@ def set_temperature2(logits, labels, iters=1, cross_validate='ece',
                     confs = np.max(softmaxs, axis=1)
                     after_temperature_ece = ECE(confs, preds, labels, bin_size = 1/num_bins)
                     """
-                    ece_criterion(self.class_temperature_scale(logits), labels).item()
+                    ece_criterion(self.class_temperature_scale2(logits, csece_temperature), labels).item()
                     
                     if acc_check:
                         _, temp_accuracy, _, _, _ = test_classification_net_logits(class_temperature_scale2(logits, csece_temperature), labels)
@@ -400,7 +400,7 @@ def set_temperature2(logits, labels, iters=1, cross_validate='ece',
             confs = np.max(softmaxs, axis=1)
             ece_list.append(ECE(confs, preds, labels, bin_size = 1/num_bins))
             """
-            ece_list.append(ece_criterion(class_temperature_scale2(logits), labels).item())
+            ece_list.append(ece_criterion(class_temperature_scale2(logits, csece_temperature), labels).item())
         """
         if cross_validate == 'ece':
             temperature = T_opt_ece
