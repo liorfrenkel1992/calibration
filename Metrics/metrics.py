bd70346b618a126174c062c5cbabcf33f0544e26
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2021-01-31 17:08:33 +0200

    Update metrics.py

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -474,7 +474,7 @@ class estECELoss(nn.Module):
     def forward(self, logits, labels):
         softmaxes = F.softmax(logits, dim=1)
         confidences, predictions = torch.max(softmaxes, 1)
-        accuracies = softmaxes[:, labels]
+        accuracies = softmaxes[labels]
         #accuracies = predictions.eq(labels)
 
         ece = torch.zeros(1, device=logits.device)
